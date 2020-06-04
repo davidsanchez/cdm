8a66b41ee917d2c232c5abb93c8b50165520b563
Darko Zaric <Darko.Zaric@fesb.hr>
2020-06-04 12:16:50 +0000

    Added ImageFITS files

---
+++ src/ImageFITS.cpp
@@ -0,0 +1,3 @@
+#include "ImageFITS.h"
+
+using namespace CCfits;
---
+++ src/ImageFITS.h
@@ -0,0 +1,6 @@
+#ifndef ImageFITS_H_
+#define ImageFITS_H_
+
+#include <CCfits>   //for manipulating FITS files
+
+#endif //  ImageFITS_H_
