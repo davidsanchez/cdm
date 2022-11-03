82925ebed3f23d81952f23d7b9af82d712d265f1
david <david.sanchez@lapp.in2p3.fr>
2022-11-03 13:43:04 +0100

    automatisation of the start of the CDM

--- payload/configurations/.bashrc
+++ payload/configurations/.bashrc
@@ -18,3 +18,5 @@ source /home/cdmmgr/MOS.conf
 
 alias ti='ls -lrth'
 
+crond
+crontab CDMCrontab
---
+++ payload/configurations/Run_cron_CDM.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source /home/cdmmgr/MOS.conf
+PROCESS="MOS_Device -d Unit_CDM_Model.xml -p 48011 -i"
+
+count=`ps aux | grep -v grep | grep "$PROCESS" | awk {'print $5'} | wc -l`
+if [ "$count" = 0 ]; then
+        echo "No Serveur_OPCUA instance, starting..."
+        cd /home/cdmmgr/cdm/
+        fuser -k -n tcp 48011
+        ./RunCDM.sh
+fi
