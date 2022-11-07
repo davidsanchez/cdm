a08905745f0d3da53e47a8fb61076fcd0e788b1e
david <david.sanchez@lapp.in2p3.fr>
2022-11-07 16:09:05 +0100

    add run docker file

--- RunDocker.sh
+++ RunDocker.sh
@@ -7,4 +7,6 @@ if [ "$count" = 0 ]; then
         # cd /home/cdmmgr/cdm/
         # fuser -k -n tcp 48011
         docker-compose -f /home/shifter/david_tmp/cdm/containers/compose/Prod-CDM-config.yaml up -d
+        sleep 20
+        docker exec -ti MOS-CDM-COMPOSE bash& > /dev/null 
 fi
