ab1238584df349f185a44afa7242b5c9e213c787
david <david.sanchez@lapp.in2p3.fr>
2022-11-07 15:58:21 +0100

    add run docker file

--- RunDocker.sh
+++ RunDocker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROCESS="MOS-CDM-COMPOSE"
 
-count=`ps aux | grep -v grep | grep "$PROCESS" | awk {'print $5'} | wc -l`
+count=`docker ps | grep -v grep | grep "$PROCESS" | awk {'print $5'} | wc -l`
 if [ "$count" = 0 ]; then
         echo "No docker image"
         # cd /home/cdmmgr/cdm/
