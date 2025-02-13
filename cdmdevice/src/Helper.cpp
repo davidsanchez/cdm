c5e46cec6d78fa21309b93b7bd4c5c495d41c8e6
david.sanchez@cta-consortirum.org <david.sanchez@cta-consortirum.org>
2025-02-13 15:29:13 +0100

    fixe

--- cdmdevice/src/Helper.cpp
+++ cdmdevice/src/Helper.cpp
@@ -11,9 +11,7 @@
 
 using namespace std;
 
-
-
-std::string  Helper::searchDatapoint (string element, Config cdm_config)
+std::string  Helper::searchDatapoint (string element, config cdm_config)
 {
         ListElement *myElement = NULL;
         std::string rootElement = "";
