53935200676494c86309241bcc7b6f75b99c7018
david.sanchez@cta-consortirum.org <david.sanchez@cta-consortirum.org>
2025-02-24 09:50:01 +0100

    debug

--- cdmdevice/src/AsynchronousThread.cpp
+++ cdmdevice/src/AsynchronousThread.cpp
@@ -469,11 +469,11 @@ void *AsynchronousThread::run(void *params)
 
                 // Puts the FSM.transition back to 0
                 //m_dataAccessClientOPCUA->setDatapoint("Unit_CDM.AuxControl.FSM.transition", 2, 0);
-                m_dataAccessClientOPCUA->setDatapoint(searchDatapoint("CDM_FSM_transition"), 2, 0);
+                m_dataAccessClientOPCUA->setDatapoint(searchDatapoint("CDM_FSM_transition",cdm_config), 2, 0);
 
                 // Puts the FSM.state back to the initial one if the state was Ready(1) or Tpoint(3)
                 if ((FSM_state == 1) || (FSM_state == 3))
-                	m_dataAccessClientOPCUA->setDatapoint(searchDatapoint("CDM_FSM_state"), 2, FSM_state);
+                	m_dataAccessClientOPCUA->setDatapoint(searchDatapoint("CDM_FSM_state",cdm_config), 2, FSM_state);
                     //m_dataAccessClientOPCUA->setDatapoint("Unit_CDM.AuxControl.FSM.state", 2, FSM_state);
             }
         }
