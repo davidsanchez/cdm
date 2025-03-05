a7cad5396a2f6374aa4cd4791c7f41f02ea4f7be
david.sanchez@cta-consortirum.org <david.sanchez@cta-consortirum.org>
2025-03-05 10:17:20 +0100

    config

--- cdmdevice/src/ConfigCDM.cpp
+++ cdmdevice/src/ConfigCDM.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 #define M 12
 // #define N 2
-char *pPath;
-pPath = std::getenv("HOME");
+
+char *pPath = std::getenv("HOME");
 #define CONFIG_FILE_PATH  std::string(pPath) +"/cdmdevice/cdm/config/CDM.config"
 #define LEDLOC_FILE_PATH  std::string(pPath) +"/cdmdevice/cdm/config/LedLoc.config"
 #define OARLLOC_FILE_PATH  std::string(pPath) +"/cdmdevice/cdm/config/OARLLoc.config"
