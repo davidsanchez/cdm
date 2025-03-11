19a98b365b053891d39477f10fee51384aea5fd3
david.sanchez@cta-consortirum.org <david.sanchez@cta-consortirum.org>
2025-03-11 11:49:42 +0100

    same

--- cdmdevice/src/AsynchronousThread.cpp
+++ cdmdevice/src/AsynchronousThread.cpp
@@ -450,9 +450,9 @@ void *AsynchronousThread::run(void *params)
                 m_dataAccessClientOPCUA->setDatapoint(helper.searchDatapoint("exposure",cdm_config), 2, boost::any_cast<double>(configure_settings[2]));
                 m_dataAccessClientOPCUA->setDatapoint(helper.searchDatapoint("gain",cdm_config), 2, boost::any_cast<int>((configure_settings[3])));
                 std::cout<<"DAVID "<<helper.searchDatapoint("pixelFormat",cdm_config)<<std::endl;
-                m_dataAccessClientOPCUA->setDatapoint(helper.searchDatapoint("pixelFormat",cdm_config), 2, boost::any_cast<string>(configure_settings[4]));
+                //m_dataAccessClientOPCUA->setDatapoint(helper.searchDatapoint("pixelFormat",cdm_config), 2, boost::any_cast<string>(configure_settings[4]));
                 //                                       Unit_CDM.AuxControl.CDM.Camera.pixelFormat.pixelFormat_v
-                //m_dataAccessClientOPCUA->setDatapoint("Unit_CDM.AuxControl.CDM.Camera.pixelFormat.pixelFormat_v", 2, boost::any_cast<string>(configure_settings[4]));
+                m_dataAccessClientOPCUA->setDatapoint("Unit_CDM.AuxControl.CDM.Camera.pixelFormat.pixelFormat_v", 2, boost::any_cast<string>(configure_settings[4]));
 
 
                 // you can put the outputs arguments in this place to inform the server
