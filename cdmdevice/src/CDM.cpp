8df931a0a41465ccc93a53b6a689fc9c2b91bf7f
david.sanchez@cta-consortirum.org <david.sanchez@cta-consortirum.org>
2025-03-11 14:58:37 +0100

    debug DB

--- cdmdevice/src/CDM.cpp
+++ cdmdevice/src/CDM.cpp
@@ -159,7 +159,6 @@ int CDM::subscribe_DataBroker()
     if (CDM::connection_result_DataBroker != -1)
     {
         cout << "Subscribing to DataBroker datapoints." << endl;
-        //cout << CDM::dp_monitor_DataBroker->getNameSpaces()<<endl;
         //delete CDM::dp_monitor_SG;
         //CDM::dp_monitor_SG = new DatapointMonitor(this);
         if (CDM::dp_monitor_DataBroker == NULL)
@@ -167,8 +166,8 @@ int CDM::subscribe_DataBroker()
             CDM::dp_monitor_DataBroker = new DatapointMonitor(this);
 
             helper.get_client_DataBroker()->subscribe(
-                CDM::dp_monitor_DataBroker->getElements(),2,
-                //CDM::dp_monitor_DataBroker->getNameSpaces(),
+                CDM::dp_monitor_DataBroker->getElements(),
+                CDM::dp_monitor_DataBroker->getNameSpaces(),
                 CDM::dp_monitor_DataBroker);
         }
 
