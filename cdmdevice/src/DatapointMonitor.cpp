3c6332de5d99c94f2c822f3d805fa02ba7b1d39b
david.sanchez@cta-consortirum.org <david.sanchez@cta-consortirum.org>
2025-03-12 14:44:21 +0100

    hardcoded

--- cdmdevice/src/DatapointMonitor.cpp
+++ cdmdevice/src/DatapointMonitor.cpp
@@ -393,7 +393,7 @@ void DatapointMonitor::CheckSISUpdate(std::vector<std::string> listElements, std
 
     int target_index;
 
-    auto sis_iterator = std::find(listElements.begin(), listElements.end(), "rr");
+    auto sis_iterator = std::find(listElements.begin(), listElements.end(), "DataBroker.DataBrokerControl.CameraControl.ModulesStatus.SIS.SIS_v");
     //auto sis_iterator = std::find(listElements.begin(), listElements.end(), this->sis_var_name);
 
     if (sis_iterator != listElements.end())
