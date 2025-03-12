ecfffe1ed3bba14c643a7563e18e2fcbe09197c8
david.sanchez@cta-consortirum.org <david.sanchez@cta-consortirum.org>
2025-03-12 15:23:52 +0100

    log

--- cdmdevice/src/DatapointMonitor.cpp
+++ cdmdevice/src/DatapointMonitor.cpp
@@ -92,6 +92,7 @@ std::vector<std::string> DatapointMonitor::getElements()
     elements.push_back(this->aux_DMWestBottom_var_name);
     elements.push_back(this->aux_DMWestTop_var_name);
     elements.push_back(this->sis_var_name);
+    
 
     return elements;
 }
