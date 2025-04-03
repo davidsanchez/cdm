87e2f0386a879a0fefc989c782e8d79e13fdf203
david.sanchez@cta-consortirum.org <david.sanchez@cta-consortirum.org>
2025-04-03 13:58:00 +0100

    fix cron

--- payload/configurations/Run_cron_CDM.sh
+++ payload/configurations/Run_cron_CDM.sh
@@ -5,7 +5,7 @@ PROCESS="MOS_Device -d Unit_CDM_Model.xml -p 48011 -i"
 count=`ps aux | grep -v grep | grep "$PROCESS" | awk {'print $5'} | wc -l`
 if [ "$count" = 0 ]; then
         echo "No Serveur_OPCUA instance, starting..."
-        cd /home/cdmmgr/cdmdevice/
+        cd /home/cdmmgr/
         fuser -k -n tcp 48011
         ./RunCDM.sh
 fi
