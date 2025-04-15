26f3fdb6e7df5bcc5280e47b3e18883cecd25a06
david.sanchez@cta-consortirum.org <david.sanchez@cta-consortirum.org>
2025-04-15 17:53:09 +0200

    disconnect and put to fsm=0

--- cdmdevice/src/CDM.cpp
+++ cdmdevice/src/CDM.cpp
@@ -621,7 +621,10 @@ int CDM::get(const std::string &chain, int commandStringAck, std::vector<boost::
         int FSM_state;
         getDataAccessClientOPCUARef()->getDatapoint(helper.searchDatapoint("state",cdm_config), 2, FSM_state);
         std::cout << "CDM::get(): state is " << FSM_state<< endl;
-
+        if (FSM_state != 0) {
+            camera.Disconnect();
+            getDataAccessClientOPCUARef()->getDatapoint(helper.searchDatapoint("state",cdm_config), 2, 0);
+        }
         
     }
 
