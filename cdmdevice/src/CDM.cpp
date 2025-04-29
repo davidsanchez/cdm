5ccc3a9d896afc275c455934d79e10b73233edb7
david.sanchez@cta-consortirum.org <david.sanchez@cta-consortirum.org>
2025-04-29 09:36:23 +0200

    HeartBeat

--- cdmdevice/src/CDM.cpp
+++ cdmdevice/src/CDM.cpp
@@ -612,8 +612,15 @@ int CDM::get(const std::string &chain, int commandStringAck, std::vector<boost::
     if (chain.find("get_HeartBeatError") != std::string::npos)
     {
         bool HeartBeatError = false;
+        bool HeartBeatEnable = false;
+        bool HeartBeat = false;
+        getDataAccessClientOPCUARef()->getDatapoint("Unit_CDM.Diagnostics._Heart_Beat", 2, HeartBeat);
+        std::cout << "CDM::get(): HeartBeat " << HeartBeat<< endl;
+        getDataAccessClientOPCUARef()->getDatapoint("Unit_CDM.Diagnostics._Enable_Heart_Beat", 2, HeartBeatEnable);
+        std::cout << "CDM::get(): HeartBeatEnable " << HeartBeatEnable<< endl;
         getDataAccessClientOPCUARef()->getDatapoint("Unit_CDM.Diagnostics._Error_Heart_Beat", 2, HeartBeatError);
         std::cout << "CDM::get(): get_HeartBeatError " << HeartBeatError<< endl;
+        /Root/Objects/2:Unit_CDM/2:Diagnostics/2:
         if (HeartBeatError) {ret = 1;}
         else {ret = 0;}
 
