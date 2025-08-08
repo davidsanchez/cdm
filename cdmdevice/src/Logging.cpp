5be7e1c4698a612642ef9d225119b955d3d2f475
david.sanchez@cta-consortirum.org <david.sanchez@cta-consortirum.org>
2025-08-08 09:46:05 +0200

    debug log

--- cdmdevice/src/Logging.cpp
+++ cdmdevice/src/Logging.cpp
@@ -8,7 +8,7 @@ namespace attrs = boost::log::attributes;
 namespace keywords = boost::log::keywords;
 
 // Définition de la variable booléenne
-bool enable_debug_logging = false;
+bool enable_debug_logging = true;
 
 //Defines a global logger initialization routine
 BOOST_LOG_GLOBAL_LOGGER_INIT(my_logger, logger_t)
