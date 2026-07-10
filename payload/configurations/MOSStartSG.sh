970998db5be8ceb5c7524ed4f9e8be2b93349d25
ricrando <ricrando@gmail.com>
2026-07-10 16:02:07 +0200

    test docker config for SG

---
+++ payload/configurations/MOSStartSG.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#Source global definitions
+if [ -f /etc/bashrc ]; then
+  . /etc/bashrc
+fi
+
+echo "Starting ssh server ..."
+/usr/sbin/sshd -D &
+
+# ADD MOS setup
+source /home/mosmgr/MOS.conf
+rm /var/run/crond.pid
+crond
+#crontab -l |
+
+echo "* * * * * /home/cdmmgr/Run_cron_SG.sh > /home/cdmmgr/cdm/SG_log.txt 2>&1"; | crontab -
+
+#{ cat; echo "* * * * * /home/cdmmgr/Run_cron_CDM.sh > /home/cdmmgr/cdm/CDM_log.txt 2>&1"; } | crontab -
+
+tail -f /dev/null
+
