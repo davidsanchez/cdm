b9ecfb33d8ed31662e60baae54365368ddfa5f37
sanchez <david.sanchez@cta-consortium.org>
2026-08-26 10:25:44 +0200

    work on transition

--- cdmdevice/src/PluginCDM.cpp
+++ cdmdevice/src/PluginCDM.cpp
@@ -316,7 +316,11 @@ int PluginCDM::cmd(const std::string& parameters,
         // TODO: dans le code original cette commande était vide
         fGoToTpointFlag = true;
 
-        m_cdmController->set_FSM_in_transition(0);
+                    
+        std::thread([this]() {
+        
+                m_cdmController->set_FSM_in_transition(0);
+            }).detach();
         start(NULL);
     }
 
